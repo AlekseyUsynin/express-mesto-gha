e9d6c5ad5f3914d32dce914767e593771b1b734e
Aleksey Usynin <fesskagf@yandex.ru>
2023-05-16 12:43:59 +0400

    customize GET and POST queries

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true
   // useCreateIndex: true,
   // useFindAndModify: false
+});
+
+app.get('/users', (req, res) => {
+  if (!users[req.params.id]) {
+    res.send({ error: 'Такого пользователя нет' });
+    return;
+  }
+  res.send(users)
+});
+
+app.get('/users/:userID', (req, res) => {
+  res.send(req.params.id)
+});
+
+app.post('/users', (req, res) => {
+  const { name, about } = req.body;
+  res.send(`Имя: ${name}, обо мне: ${about}`);
 });
 
 app.listen(PORT);
