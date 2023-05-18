2dcd09fb6d4c1ef73cc1c4dfdcfc448d4006ee57
Aleksey Usynin <fesskagf@yandex.ru>
2023-05-18 11:56:45 +0400

    configure editing user profile and avatar

--- controllers/users.js
+++ controllers/users.js
@@ -28,3 +28,37 @@ module.exports.createUser = (req, res) => {
     .then((user) => {res.send({ data: user })})
     .catch((err) => res.status(500).send({message: 'Ошибка сервера!'}));
 };
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  UserSchema
+    .findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      { new: true },
+    )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({message: 'Пользователь не найден'})
+      }
+      res.send(user)
+    })
+    .catch((err) => res.status(500).send({message: 'Ошибка сервера!'}));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  UserSchema
+    .findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true },
+    )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({message: 'Пользователь не найден'})
+      }
+      res.send(user)
+    })
+    .catch((err) => res.status(500).send({message: 'Ошибка сервера!'}));
+};
--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,18 @@
 const router = require("express").Router();
 
-const { getUsers, getUserId, createUser } = require("../controllers/users");
+const { getUsers, getUserId, createUser, updateUser, updateAvatar } = require("../controllers/users");
 const { getCards, createCard, deleteCard } = require("../controllers/cards");
 
 router.get("/users", getUsers);
 router.get("/users/:userId", getUserId);
 router.post("/users", createUser);
+router.patch("/users/me", updateUser);
+router.patch("/users/me/avatar", updateAvatar);
 
 router.get("/cards", getCards);
 router.post("/cards", createCard);
 router.delete("/cards/:cardId", deleteCard);
-
+router.put("/cards/:cardId/likes");
+router.delete("/cards/:cardId/likes");
 
 module.exports = router;
