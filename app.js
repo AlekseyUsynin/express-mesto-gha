74e3b6775a3741f9903aa576a63dd1a749afa983
Aleksey Usynin <fesskagf@yandex.ru>
2023-05-18 23:49:20 +0400

    fix error

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 //static зайдет в папку public, найдет там index.html и запустит его.
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Не раблотает с порта mongodb://localhost:27017
 // решение: https://www.mongodb.com/community/forums/t/mongooseserverselectionerror-connect-econnrefused-127-0-0-1-27017/123421/2
 mongoose.connect(
-  "mongodb://localhost:27017/mestodb"
+  "mongodb://127.0.0.1:27017/mestodb"
   // {
   // useNewUrlParser: true,
   // useCreateIndex: true,
