ad24f8027676cfb981a2cacd524f82f74a7ac535
Aleksey Usynin <fesskagf@yandex.ru>
2023-05-24 00:27:05 +0400

    fix card deletion

--- controllers/cards.js
+++ controllers/cards.js
@@ -31,7 +31,7 @@ module.exports.deleteCard = (req, res, next) => {
       if (!card) {
         throw new NotFound('Карточка с таким id не найдена.');
       }
-      if (!(req.user._id === card.owner._id)) {
+      if (req.user._id.toString() !== card.owner._id.toString()) {
         throw new Forbidden('Нельзя удалить карточку другого пользователя!');
       }
       return card
