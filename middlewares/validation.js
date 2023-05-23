0ec63db8459b9c923cd3860ee5aedb8606133a45
Aleksey Usynin <fesskagf@yandex.ru>
2023-05-24 00:39:56 +0400

    fix

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,6 +3,9 @@ const validator = require('validator');
 const BadRequest = require('../errors/BadRequest');
 
 const urlJoi = (url) => {
+  // проверяем через validator.isURL (https://www.npmjs.com/package/validator)
+  // проверяет, является ли строка URL-адресом
+  // решение от сюда: https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
   if (validator.isURL(url)) return url;
   throw new BadRequest('Не верный URL');
 };
